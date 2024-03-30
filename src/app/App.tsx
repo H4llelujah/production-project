49e2adb61b7d8887b9f37b3381d413c6791cce15
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-03-30 19:04:44 +0500

    AddAppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,22 @@
-import { Link, Route, Routes } from 'react-router-dom';
-import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
+import { AppRouter } from './providers/router';
+import { Navbar } from 'widget/Navbar';
 
 const App = () => {
 
-  const {theme, toogleTheme} = useTheme();
+  const { theme, toogleTheme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-        <button onClick={toogleTheme}>TOGGLE</button>
-        <Link to={'/'}>Главная</Link>
-        <Link to={'/about'}>О сайте</Link>
-        <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-              <Route path='/about' element={<AboutPage/>}/>
-              <Route path='/' element={<MainPage/>}/>
-          </Routes>
-        </Suspense>
+      <Navbar />
+      <AppRouter />
+      <button onClick={toogleTheme}>TOGGLE</button>
     </div>
   )
 }
 
 export default App;
+
