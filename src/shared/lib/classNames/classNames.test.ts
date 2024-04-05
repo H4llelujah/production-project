1e731446608f58a103413fbd36ae3e47e58e949e
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-05 23:41:33 +0500

    addTestCases

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,39 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only one first param', () => {
+        expect(classNames('className')).toBe('className');
+    });
+
+    test('with additional class', () => {
+        const exptected = 'className class1 class2'
+        expect(classNames('className', {}, ['class1', 'class2'])).toBe(exptected);
+    });
+
+    test('With true mods params', () => {
+        const exptected = 'className class1 target checked';
+        expect(classNames(
+            'className',
+            { target: true, checked: true },
+            ['class1'],
+        )).toBe(exptected);
+    });
+
+    test('With false mods params', () => {
+        const exptected = 'className class1 target';
+        expect(classNames(
+            'className',
+            { target: true, checked: false },
+            ['class1'],
+        )).toBe(exptected);
+    });
+
+    test('With undefined mods params', () => {
+        const exptected = 'className class1 target';
+        expect(classNames(
+            'className',
+            { target: true, checked: undefined },
+            ['class1'],
+        )).toBe(exptected);
+    });
+});
