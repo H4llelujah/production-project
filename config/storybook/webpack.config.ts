8b62c0b483a905cf05784502998ec3cdda658aa5
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-10 04:35:23 +0500

    add SVG loader into Storybook webpack config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,8 +1,7 @@
-import webpack, { RuleSetRule, util } from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/BuildCssLoader';
-
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
@@ -14,6 +13,17 @@ export default ({ config }: {config: webpack.Configuration}) => {
 
     if (config.module?.rules) {
         config.module.rules.push(buildCssLoader(true));
+        // eslint-disable-next-line no-param-reassign
+        config.module.rules = config.module.rules.map((rule: RuleSetRule | '...') => {
+            if ((typeof rule === 'object') && (/svg/.test(rule.test as string))) {
+                return { ...rule, exclude: /\.svg$/i };
+            }
+            return rule;
+        });
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
     }
 
     config.resolve?.modules?.push(paths.src);
