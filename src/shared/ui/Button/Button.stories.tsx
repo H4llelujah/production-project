9d5a99a690956e8c34a4e665866c3703a7c578d1
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-09 21:32:50 +0500

    add first story for button

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button, ThemeButton } from './Button';
+
+const meta: Meta<typeof Button> = {
+    title: 'Shared/Button',
+    component: Button,
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+    args: {
+        children: 'text',
+    },
+};
+
+export const Clear: Story = {
+    args: {
+        children: 'text',
+        theme: ThemeButton.CLEAR,
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        children: 'text',
+        theme: ThemeButton.OUTLINE,
+    },
+};
