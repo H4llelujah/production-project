c825478d3b30aee256305c22e1ccb5afaf11571c
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-11 03:17:47 +0500

    add stories for new button state and theme

--- src/shared/ui/Button/Button.stories.tsx
+++ src/shared/ui/Button/Button.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button, ThemeButton } from './Button';
+import { Button, ButtonSize, ButtonTheme } from './Button';
 
 const meta: Meta<typeof Button> = {
     title: 'Shared/Button',
@@ -19,13 +19,99 @@ export const Primary: Story = {
 export const Clear: Story = {
     args: {
         children: 'text',
-        theme: ThemeButton.CLEAR,
+        theme: ButtonTheme.CLEAR,
     },
 };
 
 export const Outline: Story = {
     args: {
         children: 'text',
-        theme: ThemeButton.OUTLINE,
+        theme: ButtonTheme.OUTLINE,
+    },
+};
+
+export const OutlineSizeM: Story = {
+    args: {
+        children: 'text',
+        theme: ButtonTheme.OUTLINE,
+        size: ButtonSize.M,
+    },
+};
+
+export const OutlineSizeL: Story = {
+    args: {
+        children: 'text',
+        theme: ButtonTheme.OUTLINE,
+        size: ButtonSize.L,
+    },
+};
+
+export const OutlineSizeXL: Story = {
+    args: {
+        children: 'text',
+        theme: ButtonTheme.OUTLINE,
+        size: ButtonSize.XL,
+    },
+};
+
+export const Background: Story = {
+    args: {
+        children: 'text',
+        theme: ButtonTheme.BACKGROUND,
+    },
+};
+
+export const BackgroundInverted: Story = {
+    args: {
+        children: 'text',
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+    },
+};
+
+export const SquareSizeM: Story = {
+    args: {
+        children: '>',
+        square: true,
+        size: ButtonSize.M,
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+    },
+};
+
+export const SquareSizeL: Story = {
+    args: {
+        children: '>',
+        square: true,
+        size: ButtonSize.L,
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+    },
+};
+
+export const SquareSizeXL: Story = {
+    args: {
+        children: '>',
+        square: true,
+        size: ButtonSize.XL,
+        theme: ButtonTheme.BACKGROUND_INVERTED,
+    },
+};
+
+export const SizeM: Story = {
+    args: {
+        children: 'text',
+        size: ButtonSize.M,
+    },
+};
+
+export const SizeL: Story = {
+    args: {
+        children: 'text',
+        size: ButtonSize.L,
+    },
+};
+
+export const SizeXL: Story = {
+    args: {
+        children: 'text',
+        size: ButtonSize.XL,
     },
 };
