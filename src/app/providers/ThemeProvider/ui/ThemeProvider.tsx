6f3ff384aaa0963605f688f48ce0ec678c417c69
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-12 21:17:37 +0500

    refactor theme Provider for storybook

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,10 +1,28 @@
-import { FC, useMemo, useState } from 'react';
+import {
+    FC, useEffect, useMemo, useState,
+} from 'react';
 import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from '../lib/ThemeContext';
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
-const ThemeProvider: FC = ({ children }) => {
-    const [theme, setTheme] = useState<Theme>(defaultTheme);
+interface ThemeProviderProps {
+    initialTheme?: Theme;
+}
+
+const ThemeProvider: FC<ThemeProviderProps> = (props) => {
+    const {
+        children,
+        initialTheme,
+    } = props;
+
+    const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
+
+    // for storybook!
+    useEffect(() => {
+        if (initialTheme) {
+            setTheme(initialTheme);
+        }
+    }, [initialTheme]);
 
     const defaultProps = useMemo(() => ({
         theme,
