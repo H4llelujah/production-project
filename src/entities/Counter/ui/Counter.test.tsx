de0395e5a2e37474aa624739bd38a20043939122
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-16 20:11:58 +0500

    add redux provider, create store, add new entity ( Counter ). add tests for Counter and configure jest for redux store

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,28 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+    const user = userEvent.setup();
+    test('Render Counter', () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    });
+    test('Increment', async () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        await user.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+    test('Decrement', async () => {
+        componentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        await user.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+    });
+});
