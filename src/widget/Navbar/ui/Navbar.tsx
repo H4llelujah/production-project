7177f344d2609494c5d014f3e06cfbee7d819bff
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-18 15:10:11 +0500

    add new feature. LoginByUsername

--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
-import { Modal } from 'shared/ui/Modal/Modal';
+import { LoginModal } from 'features/AuthByUsername';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -14,8 +14,12 @@ export const Navbar = ({ className }: NavbarProps) => {
 
     const [isAuthModal, setIsAuthModal] = useState(false);
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev);
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false);
+    }, []);
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true);
     }, []);
 
     return (
@@ -23,12 +27,14 @@ export const Navbar = ({ className }: NavbarProps) => {
             <div className={cls.links}>
                 <Button
                     theme={ButtonTheme.CLEAR_INVERTED}
-                    onClick={onToggleModal}
+                    onClick={onShowModal}
                 >
                     {t('Войти')}
                 </Button>
-                {/* eslint-disable-next-line */}
-                <Modal isOpen={isAuthModal} onClose={onToggleModal}>/auth modal will be here/</Modal>
+                <LoginModal
+                    isOpen={isAuthModal}
+                    onClose={onCloseModal}
+                />
             </div>
         </div>
     );
