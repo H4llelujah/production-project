5354d6d674ecc0227d20a5a6047b916df0afe7a3
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-19 20:25:55 +0500

    refactorEslintRules

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
         'i18next/no-literal-string': [
             'error', { markupOnly: true, ignoreAttribute: ['to', 'data-testid'] },
         ],
-        'max-len': ['error', { ignoreComments: true, code: 100 }],
+        'max-len': ['error', { ignoreComments: true, code: 120 }],
         'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
         'jsx-a11y/no-static-element-interactions': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
