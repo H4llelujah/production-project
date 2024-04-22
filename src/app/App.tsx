4461df1558c11775458e32d61a679e026d3e04fe
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-22 19:53:16 +0500

    add login user navbar and user login and logout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,20 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widget/Navbar';
 import { Sidebar } from 'widget/Sidebar';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
+import { userActions } from 'entities/User';
 import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
+import { useAppDispatch } from './providers/StoreProvider';
 
 const App = () => {
     const { theme } = useTheme();
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.initAuthData());
+    }, [dispatch]);
 
     return (
         <div className={classNames('app', {}, [])}>
--- src/widget/Navbar/ui/Navbar.tsx
+++ src/widget/Navbar/ui/Navbar.tsx
@@ -3,6 +3,9 @@ import { useTranslation } from 'react-i18next';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 import { useCallback, useState } from 'react';
 import { LoginModal } from 'features/AuthByUsername';
+import { useAppDispatch } from 'app/providers/StoreProvider';
+import { useSelector } from 'react-redux';
+import { getUserAuthData, userActions } from 'entities/User';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -11,6 +14,10 @@ interface NavbarProps {
 
 export const Navbar = ({ className }: NavbarProps) => {
     const { t } = useTranslation();
+
+    const dispatch = useAppDispatch();
+
+    const authData = useSelector(getUserAuthData);
 
     const [isAuthModal, setIsAuthModal] = useState(false);
 
@@ -21,6 +28,25 @@ export const Navbar = ({ className }: NavbarProps) => {
     const onShowModal = useCallback(() => {
         setIsAuthModal(true);
     }, []);
+
+    const onLogout = useCallback(() => {
+        dispatch(userActions.logout());
+    }, [dispatch]);
+
+    if (authData) {
+        return (
+            <div className={classNames(cls.Navbar, {}, [className])}>
+                <div className={cls.links}>
+                    <Button
+                        theme={ButtonTheme.CLEAR_INVERTED}
+                        onClick={onLogout}
+                    >
+                        {t('Выйти')}
+                    </Button>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <div className={classNames(cls.Navbar, {}, [className])}>
