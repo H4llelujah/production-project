32936fb514f3c96c0cd579e2a554be8295bd8cd4
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-04-23 21:46:54 +0500

    refactor some issues for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,4 +1,4 @@
-import webpack, { RuleSetRule } from 'webpack';
+import webpack, { DefinePlugin, RuleSetRule } from 'webpack';
 import path from 'path';
 import { BuildPaths } from '../build/types/config';
 import { buildCssLoader } from '../build/loaders/BuildCssLoader';
@@ -28,6 +28,10 @@ export default ({ config }: {config: webpack.Configuration}) => {
 
     config.resolve?.modules?.push(paths.src);
     config.resolve?.extensions?.push('.ts', '.tsx');
+
+    config.plugins?.push(new DefinePlugin({
+        __IS_DEV__: true,
+    }));
 
     return config;
 };
