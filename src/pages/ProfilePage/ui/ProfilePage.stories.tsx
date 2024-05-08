8c8042c8e58961861c8f015905e101fbc8d46c70
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-05-08 22:14:16 +0500

    add profilePage

---
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ProfilePage from './ProfilePage';
+
+const meta: Meta<typeof ProfilePage> = {
+    title: 'Pages/ProfilePage',
+    component: ProfilePage,
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof ProfilePage>;
+
+export const Primary: Story = {};
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,6 +1,12 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
+import { DynamicModlueLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModlueLoader';
+import { profileReducer } from 'entities/Profile';
 import cls from './ProfilePage.module.scss';
+
+const reducers: ReducerList = {
+    profile: profileReducer,
+};
 
 interface ProfilePageProps {
     className?: string;
@@ -9,9 +15,11 @@ interface ProfilePageProps {
 const ProfilePage = ({ className }: ProfilePageProps) => {
     const { t } = useTranslation();
     return (
-        <div className={classNames('', {}, [className])}>
-            {t('Profile page')}
-        </div>
+        <DynamicModlueLoader reducers={reducers} removeAfterUnmount>
+            <div className={classNames('', {}, [className])}>
+                {t('Profile page')}
+            </div>
+        </DynamicModlueLoader>
     );
 };
 
