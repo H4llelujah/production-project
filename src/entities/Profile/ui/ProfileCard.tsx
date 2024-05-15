893d07d5c92106efad49d8b3d219ca89aa974a3c
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-05-15 20:57:18 +0500

    fix ProfileCard trouble

--- src/entities/Profile/ui/ProfileCard.tsx
+++ src/entities/Profile/ui/ProfileCard.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import { Text } from 'shared/ui/Text/Text';
 import { Button } from 'shared/ui/Button/Button';
 import { Input } from 'shared/ui/Input/Input';
+import { Loader } from 'shared/ui/Loader/Loader';
 import cls from './ProfileCard.module.scss';
 import { getProfileData } from '../model/selectors/getProfileData/getProfileData';
 import { getProfileError } from '../model/selectors/getProfileError/getProfileError';
@@ -18,6 +19,11 @@ export const ProfileCard = ({ className }: ProfileCardProps) => {
     const data = useSelector(getProfileData);
     const error = useSelector(getProfileError);
     const isLoading = useSelector(getProfileIsLoading);
+    if (isLoading) {
+        return (
+            <Loader />
+        );
+    }
     return (
         <div className={classNames(cls.ProfileCard, {}, [className])}>
             <div className={cls.header}>
