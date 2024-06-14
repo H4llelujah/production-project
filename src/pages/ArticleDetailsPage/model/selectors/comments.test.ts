463d0c4642078a327013e4d7b7b77bb5fada0e52
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-06-14 18:41:54 +0500

    add selectors tests

---
+++ src/pages/ArticleDetailsPage/model/selectors/comments.test.ts
@@ -0,0 +1,29 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+import { getArticleCommentsError, getArticleCommentsIsLoading } from './comments';
+
+describe('commentsSelectors', () => {
+    test('get isLoading true status', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetailsComments: {
+                isLoading: true,
+            },
+        };
+        expect(getArticleCommentsIsLoading(state as StateSchema)).toBe(true);
+    });
+    test('get isLoading false status', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleCommentsIsLoading(state as StateSchema)).toBe(false);
+    });
+    test('get error status', () => {
+        const state: DeepPartial<StateSchema> = {
+            articleDetailsComments: {
+                error: 'error',
+            },
+        };
+        expect(getArticleCommentsError(state as StateSchema)).toBe('error');
+    });
+    test('get error status', () => {
+        const state: DeepPartial<StateSchema> = {};
+        expect(getArticleCommentsError(state as StateSchema)).toBe(undefined);
+    });
+});
