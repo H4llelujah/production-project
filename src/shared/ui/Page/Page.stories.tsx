1ecdc94e3fed5638735a0170a4a5b99439a1d4f5
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-06-20 22:13:21 +0500

    add page shared component

---
+++ src/shared/ui/Page/Page.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Page } from './Page';
+
+const meta: Meta<typeof Page> = {
+    title: 'Shared/Page',
+    component: Page,
+    tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Page>;
+
+export const Primary: Story = {
+    args: {
+        // eslint-disable-next-line i18next/no-literal-string
+        children: (<div>Hello!</div>),
+    },
+};
