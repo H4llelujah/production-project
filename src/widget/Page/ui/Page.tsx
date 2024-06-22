6009f22cf335ecaca7b6c17843fec895df889dda
Pavel Baldin <bpa25.07.2002@mail.ru>
2024-06-22 16:08:24 +0500

    use Throttle hook to follow the scroll position after changing the routes

---
+++ src/widget/Page/ui/Page.tsx
@@ -0,0 +1,61 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+import {
+    MutableRefObject,
+    ReactNode,
+    UIEvent,
+    useRef,
+} from 'react';
+import { useInfiniteScroll } from 'shared/lib/hooks/useInfiniteScroll/useInfiniteScroll';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { ScrollSaveActions, getScrollByPath } from 'widget/ScrollSave';
+import { useLocation } from 'react-router-dom';
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
+import { useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider';
+import { useThrottle } from 'shared/lib/hooks/useThrottle/useThrottle';
+import cls from './Page.module.scss';
+
+interface PageProps {
+    className?: string;
+    children: ReactNode;
+    onScrollEnd?: () => void;
+}
+
+export const Page = (props: PageProps) => {
+    const { className, children, onScrollEnd } = props;
+    const wrapperRef = useRef() as MutableRefObject<HTMLElement>;
+    const triggerRef = useRef() as MutableRefObject<HTMLDivElement>;
+    const dispatch = useAppDispatch();
+    const { pathname } = useLocation();
+    const scrollPosition = useSelector(
+        (state: StateSchema) => getScrollByPath(state, pathname),
+    );
+
+    useInfiniteScroll({
+        triggerRef,
+        wrapperRef,
+        callback: onScrollEnd,
+    });
+
+    useInitialEffect(() => {
+        wrapperRef.current.scrollTop = scrollPosition;
+    }, []);
+
+    const onScroll = useThrottle((e: UIEvent<HTMLDivElement>) => {
+        dispatch(ScrollSaveActions.setScrollPosition({
+            path: pathname,
+            position: e.currentTarget.scrollTop,
+        }));
+    }, 500);
+
+    return (
+        <section
+            ref={wrapperRef}
+            onScroll={onScroll}
+            className={classNames(cls.Page, {}, [className])}
+        >
+            {children}
+            <div ref={triggerRef} />
+        </section>
+    );
+};
